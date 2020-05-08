0883c2463e54fd96b19542e56ea301476509e636
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2020-05-08 17:59:33 -0400

    Update Blynk_WM_Template.ino

--- examples/Blynk_WM_Template/Blynk_WM_Template.ino
+++ examples/Blynk_WM_Template/Blynk_WM_Template.ino
@@ -1,14 +1,51 @@
-#define SERIAL_SPEED 230400
-#define SKETCH_NAME "Blynk_WiFiMgr_ESP32_8266_Template_HexColor"
+/****************************************************************************************************************************
+   Blynk_WM_Template.ino
+   For ESP32/ESP8266 boards
 
-#define BLYNK_PRINT Serial  // Generates Blynk debug prints. Comment out if not needed, saving space
+   Written by Thor Johnson (https://github.com/thorathome) May 2020 as a template for
+   Blynk Wifi ESP communications. See https://github.com/thorathome/Blynk_Examples
+
+   Blynk_WM is a library for the ESP8266/ESP32 Arduino platform (https://github.com/esp8266/Arduino) to enable easy
+   configuration/reconfiguration and autoconnect/autoreconnect of WiFi/Blynk
+   Forked from Blynk library v0.6.1 https://github.com/blynkkk/blynk-library/releases
+   Built by Khoi Hoang https://github.com/khoih-prog/Blynk_WM
+   Licensed under MIT license
+   Version: 1.0.14
+
+   Original Blynk Library author:
+   @file       BlynkSimpleEsp8266.h
+   @author     Volodymyr Shymanskyy
+   @license    This project is released under the MIT License (MIT)
+   @copyright  Copyright (c) 2015 Volodymyr Shymanskyy
+   @date       Jan 2015
+   @brief
+
+   Version    Modified By   Date      Comments
+   -------    -----------  ---------- -----------
+    1.0.0     K Hoang      28/10/2019 Initial coding
+    1.0.1     K Hoang      28/10/2019 Add features
+    1.0.2     K Hoang      21/11/2019 Fix bug. Add features.
+    1.0.3     K Hoang      31/11/2019 Fix compiler errors for ESP8266 core pre-2.5.2. Add examples.
+    1.0.4     K Hoang      07/01/2020 Add configurable personalized RFC-952 DHCP hostname
+    1.0.5     K Hoang      20/01/2020 Add configurable static IP, GW, SN, DNS1, DNS2 and Config Portal static IP and Credentials
+    1.0.6     K Hoang      05/02/2020 Optimize, fix EEPROM size to 2K from 4K, shorten code size, add functions
+    1.0.7     K Hoang      18/02/2020 Add checksum, enable AutoConnect to configurable MultiWiFi and MultiBlynk Credentials
+    1.0.8     K Hoang      24/02/2020 Fix AP-staying-open bug. Add clearConfigData()
+    1.0.9     K Hoang      12/03/2020 Enhance Config Portal GUI
+    1.0.10    K Hoang      08/04/2020 SSID password maxlen is 63 now. Permit special chars # and % in input data.
+    1.0.11    K Hoang      09/04/2020 Enable adding dynamic custom parameters from sketch
+    1.0.12    K Hoang      13/04/2020 Fix MultiWiFi/Blynk bug introduced in broken v1.0.11
+    1.0.13    K Hoang      25/04/2020 Add Configurable Config Portal Title, Default Config Data and DRD. Update examples.
+    1.0.14    K Hoang      03/05/2020 Fix bug and change feature in dynamicParams.
+ *****************************************************************************************************************************/
 
 // Sketch uses Arduino IDE-selected ESP32 and ESP8266 to select compile choices
 
 /*  
  * The Arduino/Blynk sketch Blynk_WiFiMgr_ESP32_8266_Template_HexColor.ino is a fully-developed 
  * get-started demo program for the powerful BlynkSimpleEsp... and the newer WiFiManager (WM) libraries. 
- * See https://github.com/thorathome/Blynk_Examples
+ * This demo sketch written by Thor Johnson (https://github.com/thorathome) May 2020 as a template for
+ * Blynk Wifi ESP communications. See https://github.com/thorathome/Blynk_Examples
  * 
  * It demonstrates
  * * WiFiManager Config Portal configuration and use
@@ -49,6 +86,10 @@
  * the BlynkSimpleEsp... and ...WiFiManager libraries, the ESP32 and ESP8266.
  */
 
+#define SERIAL_SPEED 230400
+#define SKETCH_NAME "Blynk_WiFiMgr_ESP32_8266_Template_HexColor"
+
+#define BLYNK_PRINT Serial  // Generates Blynk debug prints. Comment out if not needed, saving space
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 //// COMPILER SWITCH SELECTION - USE WIFI MANAGER OR NOT //////////////////////////////////
