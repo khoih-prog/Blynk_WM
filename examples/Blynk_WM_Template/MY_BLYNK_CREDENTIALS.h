99a2a3c771deef5dd7636206942fd2c352321f6f
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2020-06-29 17:41:10 -0400

    Update
    Update with more notes from original author

--- examples/Blynk_WM_Template/MY_BLYNK_CREDENTIALS.h
+++ examples/Blynk_WM_Template/MY_BLYNK_CREDENTIALS.h
@@ -31,10 +31,67 @@
     1.0.16    K Hoang      25/06/2020 Fix bug and logic of USE_DEFAULT_CONFIG_DATA. Auto format SPIFFS/LittleFS.
  *****************************************************************************************************************************/
 
+/* MY_BLYNK_CREDENTIALS.h
+
+   Since Blynk requires a different Blynk Authcode for every device on every Blynking panel,
+   I end up with a lot of Blynking Authcodes.
+
+   A simple way to manage all the Blynk Authcodes you will likely generate...
+   * Put them all in a library file you can #include in your Arduino sketches.
+   * Name each of the Blynk Authcodes with a helpful name.
+   * Select the right Blynk Authcode in each sketch without having to hard-code it.
+
+   Here's how...
+   * Create a new folder called MY_BLYNK_CREDENTIALS in your Arduino/libraries folder
+  * Create a new text file named MY_BLYNK_CREDENTIALS.h
+     (Same as as the folder. The .h is important.)
+
+   * Use this as a template.
+   * Edit in your own Blynk Authcodes with descriptive names for each of your projects or devices
+*/
+
+
 #ifndef MY_BLYNK_CREDENTIALS_h
 #define MY_BLYNK_CREDENTIALS_h
-
-#define MY_BLYNK_SERVER               "blynk-cloud.com"
+        
+#define MY_BLYNK_SERVER               "blynk-cloud.com"                     // You may or may not need this depending on your code
 #define MY_WM_TEMPLATE_AUTHCODE       "0123456789abcdef0123456789abcdef"
+
+// From my Blynk Panel 1 ( 3 devices )
+#define MY_EXTERIOR_TEMP_AUTHCODE     "wtxxxxxxxxxxxxxxxxxxxxxxxxxxxxBS"  // Use your real Blynk Authcode here
+
+#define MY_AC_DEV_AUTHCODE            "fnyyyyyyyyyyyyyyyyyyyyyyyyyyyy7t"  // Use your real Blynk Authcode here
+
+#define MY_BARO_AUTHCODE              "_YzzzzzzzzzzzzzzzzzzzzzzzzzzzzRP"  // Use your real Blynk Authcode here
+
+
+//From my Blynk Panel 2 ( 2 devices )
+#define MY_IFTTT_DEMO_AUTHCODE        "_cbbbbbbbbbbbbbbbbbbbbbbbbbbbbsi"  // Use your real Blynk Authcode here
+#define MY_SONOFF_DEV_AUTHCODE        "HWcccccccccccccccccccccccccccc7K"  // Use your real Blynk Authcode here
+
+
+//From my Blynk Panel 3 ( 1 device )
+#define MY_REAL_SONOFF_AUTHCODE       "Geddddddddddddddddddddddddddddqc"  // Use your real Blynk Authcode here
+
+
+/*
+ * * Once this library file is in place, restart your Arduino IDE.
+ * * In each Arduino sketch, add the line
+     #include "MY_BLYNK_AUTHCODES.h" // Use quotes " here.
+ * * In all your sketches write something like
+     #define MY_BLYNK_AUTHCODE MY_BARO_AUTHCODE //from your .h file.
+ * * Write each sketch to use MY_BLYNK_AUTHCODE throughout the sketch
+     instead of a hard-coded Blynk Authcode.
+ * * Right after the #include "MY_BLYNK_AUTHCODES.h" statement, write a #define command to choose the
+     correct Blynk Authcode.
+ * * Compile and go
+
+   This way,
+ * * All Blynk Authcodes are in one place where you can keep track of them.
+ * * You are not hard-coding Blynk Authcodes in your sketches.
+ * * You Arduino code becomes more portable because all sketches use MY_BLYNK_AUTHCODE.
+ * * If you ever have to publish your code, your Blynk Authcodes are not visible to everyone.
+ * * You will be happier in life.
+*/
 
 #endif    //MY_BLYNK_CREDENTIALS_h
--- examples/Blynk_WM_Template/MY_WIFI_CREDENTIALS.h
+++ examples/Blynk_WM_Template/MY_WIFI_CREDENTIALS.h
@@ -31,10 +31,49 @@
     1.0.16    K Hoang      25/06/2020 Fix bug and logic of USE_DEFAULT_CONFIG_DATA. Auto format SPIFFS/LittleFS.
  *****************************************************************************************************************************/
 
+/* MY_WIFI_CREDENTIALS.h
+
+   Even if you use only one SSID/WiFiPassword, you may wish to keep it in a central place, out of your code.
+
+   A simple way to manage SSIDs and WiFiPasswords you may wish to use...
+     Put your WiFi credentials in a file you can #include in your Arduino sketches.
+     Use a simple label like MY_SSID and MY_WIFI_PASSWORD in your code.
+
+   Here's how...
+     Create a new folder called MY_WIFI_CREDENTIALS in the Arduino/libraries folder
+     Create a new text file named MY_WIFI_CREDENTIALS.h
+     (Same as  as the folder. The .h is important.)
+
+     Use this as a template.
+     Edit in your own WiFi SSID(s) and WiFi Password(s)
+*/
+
 #ifndef MY_WIFI_CREDENTIALS_h
 #define MY_WIFI_CREDENTIALS_h
 
-#define MY_WIFI_SSID          "my_wifi_ssid"
-#define MY_WIFI_PASSWORD      "my_wifi_password"
+#define MY_WIFI_SSID            "Serengeti"                       // network SSID (name)
+#define MY_WIFI_PASSWORD        "hey-this is secret 9897%%&"      // network password
+
+/* Comment out the credentials you do not use as regularly...
+  #define MY_WIFI_SSID "Virus-Vault"                     // network SSID (name)
+  #define MY_WIFI_PASSWORD "TwoFourSixEight2468!"        // network password
+  #define MY_WIFI_SSID "MyAndroidPhone"                  // network SSID (name)
+  #define MY_WIFI_PASSWORD "AndroidPassword66$"          // network password
+*/
+
+/*
+ * * With the library file in place, restart your Arduino IDE.
+ * * In each Arduino sketch, add the line
+     #include "MY_WIFI_CREDENTIALS.h" // Use quotes " here.
+ * * Write each sketch to use MY_SSID and MY_WIFI_PASSWORD throughout the sketch
+ * * Compile and go
+
+   This way,
+ * * All your WiFi credentials are in one place where you can keep track of them.
+ * * You are not hard-coding passwords in your sketches.
+ * * You Arduino code becomes more portable because all sketches use MY_WIFI_CREDENTIALS.
+ * * If you ever have to publish your code, your WiFi Credentials are not visible to everyone.
+ * * You will be happier in life.
+*/
 
 #endif    //MY_WIFI_CREDENTIALS_h
